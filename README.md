732a2748817010b726e0dc3cdea4f8a94d1f779a
Tagussan <jap.yoshi@gmail.com>
2020-04-12 05:20:08 +0000

    add js; update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GoogleScholarOnUTokyoGateway
+
+## 概要
+Google Scholarのページのリンクを自動的に書き換え、UTokyo SSL-VPN gateway経由へのアクセスにするカスタムjavascriptです。[東京大学情報基盤センターが公開している内容に基づいて](https://www.sodan.ecc.u-tokyo.ac.jp/hack/search-engine-via-ssl-vpn/)書き換えを行っています。
+
+## 使い方
+1. お使いのブラウザに[Tampermonkey](https://www.tampermonkey.net/)を入れて有効化してください。
+
---
+++ auto_gateway.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name     Google_Scholar_on_UTokyo_gateway
+// @version  1
+// @include  https://scholar.google.com/*
+// @grant    none
+// ==/UserScript==
+
+function get_gateway_url(url){
+  const url_obj = new URL(url);
+  return 'https://gateway.itc.u-tokyo.ac.jp' + url_obj.pathname + url_obj.search + ',DanaInfo=' + url_obj.hostname;
+}
+
+(function() {
+  const links = Array.from(document.getElementsByTagName('a'));
+  const re_url = new RegExp('^(https?|ftp):\/\/');
+  const journal_links = links.filter(link => {
+    const href = link.href
+    return href.match(re_url) && (!href.match('google')) && link.hasAttribute("id");
+  });
+  journal_links.forEach(link => {
+    link.href = get_gateway_url(link.href);
+  });
+})();
