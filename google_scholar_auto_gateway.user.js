7c94b81a01145e2360600bf1faf9f3eb9caa7b44
Tagussan <jap.yoshi@gmail.com>
2020-04-23 03:50:49 +0000

    reject [PDF] or [HTML]

--- google_scholar_auto_gateway.user.js
+++ google_scholar_auto_gateway.user.js
@@ -3,26 +3,40 @@
 // @version  1
 // @include  https://scholar.google*
 // @grant    none
+// @require  https://code.jquery.com/jquery-3.5.0.slim.min.js
 // This software is released under the MIT License, see LICENSE.
 // ==/UserScript==
 
 function get_gateway_url(url){
   const url_obj = new URL(url);
   if(url.match('arxiv')){
-    return url; //don't use gateway for arxiv
+    return url;
   } else {
     return 'https://gateway2.itc.u-tokyo.ac.jp' + url_obj.pathname + url_obj.search + ',DanaInfo=' + url_obj.hostname;
   }
 }
 
+function article_page_block(block){
+    if($(block).find("span.gs_ctc").length == 1){ //[PDF] or [HTML]
+      return false;
+    }else{
+      return true;
+    }
+}
+
 (function() {
-  const links = Array.from(document.getElementsByTagName('a'));
   const re_url = new RegExp('^(https?|ftp):\/\/');
-  const journal_links = links.filter(link => {
-    const href = link.href
-    return href.match(re_url) && (!href.match('google')) && link.hasAttribute("id");
-  });
-  journal_links.forEach(link => {
-    link.href = get_gateway_url(link.href);
+  const article_block = $('#gs_res_ccl_mid .gs_ri');
+  article_block.each((i, e) => {
+    if(article_page_block(e)){
+      const journal_links = $(e).find("a").filter((i, link) => {
+        const href = link.href
+        return href.match(re_url) && (!href.match('google')) && link.hasAttribute("id");
+      });
+      journal_links.each((i, link) => {
+        link.href = get_gateway_url(link.href);
+      });
+    }
   });
 })();
+
